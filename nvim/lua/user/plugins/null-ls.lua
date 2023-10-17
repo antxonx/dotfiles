ff4d91dce787bab249a46b9104525ff5690bb248
alejandro antonio <aantonio@gdlconnect.com>
2023-10-16 18:12:59 -0600

    prettier config

--- nvim/lua/user/plugins/null-ls.lua
+++ nvim/lua/user/plugins/null-ls.lua
@@ -2,7 +2,7 @@ return {
   "jose-elias-alvarez/null-ls.nvim",
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require "null-ls"
 
     -- Check supported formatters and linters
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -11,6 +11,10 @@ return {
       -- Set a formatter
       -- null_ls.builtins.formatting.stylua,
       -- null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.prettier.with({
+        filetypes = { "html", "json", "css", "javascript", "typescript", "javascriptreact", "jsx" },
+        extra_args = {"--semi", "--single-quote", "--tab-width", "4"}
+      }),
     }
     return config -- return final config table
   end,
